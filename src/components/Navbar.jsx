6ce8d4204af43ae0a3432eca0515e8c47ba31f88
Roshinitula <tularoshini@gmail.com>
2025-02-13 12:44:27 +0530

    pushing files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Styles from "./navbar.module.css";
+import Styles from "./navabr.module.css";
 import Logo from './Logo';
 import Menu from './Menu';
 const Navbar = () => {
   return (
-    <section id ={Styles.navBlock}>
+    <section id={Styles.navBlock}>
         <article>
             <Logo/>
             <Menu/>
         </article>
     </section>
-
   )
 }
 
