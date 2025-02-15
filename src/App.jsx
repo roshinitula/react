90b467dc439c8213c57418adc5c252880c6f52b4
Roshinitula <tularoshini@gmail.com>
2025-02-15 12:55:22 +0530

    pushing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
+// import React from 'react'
+// import Navbar from './components/Navbar';
+// import {BrowserRouter  , Routes , Route} from "react-router-dom";
+// import Dashboard from './pages/Dashboard';
+// import Clasroom from './pages/Clasroom';
+// import Assessments from './pages/Assessments';
+// import Store from './pages/Store';
+// import Calender from './pages/Calender';
+// import News from './pages/News';
+// import Blog from './pages/Blog';
+// import PageNotFound from './pages/PageNotFound';
+// import Profile from './pages/Profile';
+// import MyAccount from './pages/MyAccount';
+// import MySettings from './pages/MySettings';
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//         <Navbar/>
+//         <Routes>
+//             <Route path='/' element={<Dashboard/>}/>
+//             <Route path='/classroom' element={<Clasroom/>}/>
+//             <Route path='/assessment' element={<Assessments/>}/>
+//             <Route path='/store' element={<Store/>}/>
+//             <Route path='/calender' element={<Calender/>}/>
+//             <Route path='/news' element={<News/>}/>
+//             <Route path='/blog' element={<Blog/>}/>
+//             <Route path='/profile' element={<Profile/>}>
+//               <Route path='/profile/myaccount' element={<MyAccount/>}/>
+//               <Route path='/profile/mysettings' element={<MySettings/>}/>
+//               </Route>
+//             <Route path='*' element={<PageNotFound/>}/>
+//         </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import { BrowserRouter,Route,Routes } from 'react-router-dom'
+// import Assessment from "./pages/Assessment"
+// import Dashboard from "./pages/Dashboard"
+// import Classrooms from "./pages/Classrooms"
+// import Store from "./pages/Store"
+// import Calender from "./pages/Calender"
+// import News from "./pages/News"
+// import Blog from "./pages/Blog"
+// import Pagenotfound from './pages/Pagenotfound'
+// import Profile from './pages/Profile'
+// import Myaccount from './pages/Myaccount'
+// import Myseetings from './pages/Myseetings'
+
+// const App = () => {
+//   return (
+//         <BrowserRouter>
+//          <Navbar/>
+//          <Routes>
+//           <Route path="/" element={<Dashboard/>}/>
+//           <Route path="/classrooms" element={<Classrooms/>}/>
+//           <Route path="/assessment" element={<Assessment/>}/>
+//           <Route path="/store" element={<Store/>}/>
+//           <Route path="/calender" element={<Calender/>}/>
+//           <Route path="/news" element={<News/>}/>
+//           <Route path="/blog" element={<Blog/>}/>
+//           <Route path="/profile" element={<Profile/>}>
+//           <Route path="/profile/accounts" element={<Myaccount/>}/>
+//           <Route path="/profile/settings" element={<Myseetings/>}/>
+//           </Route>
+//           <Route path="*" element={<Pagenotfound/>}/>
+//          </Routes>
+//          </BrowserRouter>
+    
+//   )
+// }
+
+// export default App
 import React from 'react'
-import Navbar from './components/Navbar';
-import {BrowserRouter  , Routes , Route} from "react-router-dom";
-import Dashboard from './pages/Dashboard';
-import Clasroom from './pages/Clasroom';
-import Assessments from './pages/Assessments';
-import Store from './pages/Store';
-import Calender from './pages/Calender';
-import News from './pages/News';
-import Blog from './pages/Blog';
-import PageNotFound from './pages/PageNotFound';
-import Profile from './pages/Profile';
-import MyAccount from './pages/MyAccount';
-import MySettings from './pages/MySettings';
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './components/crudcomponents/Home'
+import Edit from './components/crudcomponents/Edit'
+import Create from './components/crudcomponents/Create'
+import {ToastContainer} from "react-toastify"
 const App = () => {
   return (
     <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/classroom' element={<Clasroom/>}/>
-            <Route path='/assessment' element={<Assessments/>}/>
-            <Route path='/store' element={<Store/>}/>
-            <Route path='/calender' element={<Calender/>}/>
-            <Route path='/news' element={<News/>}/>
-            <Route path='/blog' element={<Blog/>}/>
-            <Route path='/profile' element={<Profile/>}>
-              <Route path='/profile/myaccount' element={<MyAccount/>}/>
-              <Route path='/profile/mysettings' element={<MySettings/>}/>
-              </Route>
-            <Route path='*' element={<PageNotFound/>}/>
-        </Routes>
+    <ToastContainer/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/create" element={<Create/>}/>
+    <Route path="/edit/:id" element={<Edit/>}/>
+
+    </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/components/crudcomponents/Create.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Link ,useNavigate} from 'react-router-dom';
+import axios from "axios";
+import Home from "./Home"
+import {toast} from "react-toastify"
+import "./create.css"
+
+const Create = () => {
+  const[name,setname]=useState('');
+  const[email,setEmail]=useState('');
+  const[phone,setPhone]=useState('');
+  const navigate=useNavigate();
+  
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  console.log(name,email,phone)
+  let payload={name,email,phone}
+  console.log(payload)
+  axios.post("http://localhost:8000/users",payload)
+  .then(res=>{
+    toast.success("User created")
+    navigate("/")
+  })
+  .catch(err=>{
+    toast.error("User not created");
+  })
+}
+  
+  return (
+    <div>
+      <h1 style={{color:"dodgerblue"}}>Create user</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" value={name} onChange={(e)=>{
+          setname(e.target.value)
+        }} ></input><br/><br/>
+        <input type="email" placeholder="email" value={email} onChange={(e)=>{
+          setEmail(e.target.value)
+        }}></input><br/><br/>
+        <input type="tel" placeholder="phone" maxLength={10} value={phone} onChange={(e)=>{
+          setPhone(e.target.value)
+        }}></input><br/><br/>
+        <Link to="/" element={<Home/>}/>
+        <button className="Submit">Create</button>
+        <br/>
+        <br/>
+        
+        <Link to="/" className="BackHome">Back to Homepage</Link>
+        
+        
+      </form>
+    </div>
+  )
+}
+
+export default Create
